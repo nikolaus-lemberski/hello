10c03c22b78a8d229a9df3739cf57ff150a4b058
Nikolaus Lemberski <nlemberski@pivotal.io>
2020-11-12 17:13:51 +0100

    v1

--- src/main/java/com/lemberski/demo/hello/HelloController.java
+++ src/main/java/com/lemberski/demo/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public Mono<String> sayHello() {
-        return Mono.just("Hello World");
+        return Mono.just("Hello World v1");
     }
 
 }
