49811b7c9638032bcfb61f138738852815ba1f76
Nikolaus Lemberski <nlemberski@pivotal.io>
2020-11-17 10:54:05 +0100

    adding v1

--- src/main/java/com/lemberski/demo/hello/HelloController.java
+++ src/main/java/com/lemberski/demo/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public Mono<String> sayHello() {
-        return Mono.just("Hello World v2");
+        return Mono.just("Hello World v1");
     }
 
 }
